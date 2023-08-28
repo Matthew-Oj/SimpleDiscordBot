00a274459a97543d8521e7c9860b44fbb40b3756
Matthew <edafeoj@gmail.com>
2023-08-28 13:02:20 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+import responses
+import os
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = os.getenv("TOKEN")
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+        try:
+            target_channel_id = os.getenv("CHANEL_ID")
+            target_channel = await client.fetch_channel(int(target_channel_id))
+            await target_channel.send("Hello! I'm online now.")
+        except Exception as e:
+            print("Error sending initial message:", e)
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        chanel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({chanel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Incomplete Discord Chat Bot README
+The Incomplete Discord Chat Bot is a work-in-progress project designed to respond to specific prompts in a Discord server. While currently incomplete, the bot is intended to have the ability to send messages to a designated channel based on specific triggers.
+
+## Features (Planned)
+**Prompt Responses**: The bot is designed to respond to three predetermined prompts with different messages.
+
+**Channel Interaction**: It should be able to send messages to a specified channel within the Discord server (not working as intended).
+## Usage (Current State)
+As of now, the Discord chatbot is in an incomplete state. Key feature, specific channel interaction via server ID, is not functional.
